7994627571a8af32fb962e5912f52a10a6e2d104
bash <bashhafsat@gmail.com>
2021-07-27 12:19:50 +0100

    add a build

---
+++ build/precache-manifest.5adf96bf94ebd9cf2dabf8d39703c21e.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a8a8565102fadb2a8449b783138edd02",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f60804b2fcc34ad3dc81",
+    "url": "/static/js/2.03dffb3d.chunk.js"
+  },
+  {
+    "revision": "7f43a9a9a874fa527a0f",
+    "url": "/static/js/main.a2828ca7.chunk.js"
+  },
+  {
+    "revision": "583e51a7281672351041",
+    "url": "/static/js/runtime-main.38f647cd.js"
+  }
+]);
